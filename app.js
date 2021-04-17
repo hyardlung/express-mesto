9bb896238dc6c89068c8d3f98dfa017b23079321
Dmitry Volkov <hyardlung@gmail.com>
2021-04-18 00:40:45 +0700

    feat: создать роуты и контроллеры для юзеров
    В проект добавлены директории для роутов и контроллеров. Написаны запросы:
    * На возврат всех пользователей
    * На возврат конкретного пользователя по id
    * На оздание нового пользователя
    Создание пользователя корректно добавляет новый объект с заданными данными в базу. Оттуда же пользователи корректно возвращаются по запросу.

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(express.json());
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
