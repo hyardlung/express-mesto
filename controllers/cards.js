1946f2a638cf3a8247cf8ffb1a9c79bdfe27b59d
Dmitry Volkov <hyardlung@gmail.com>
2021-04-18 16:23:17 +0700

    feat: написать запросы для лайков, редакт. профиля
    Добавил запросы серверу на постановку/снятие лайка, а также редактирование данных профиля и аватара.

--- controllers/cards.js
+++ controllers/cards.js
@@ -26,3 +26,23 @@ module.exports.deleteCardById = (req, res) => {
       }
     });
 };
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.send(card))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.send(card))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+};
