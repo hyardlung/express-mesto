320bd4bf3e4dd84bf6c3fae417d1311ea243ad89
Dmitry Volkov <hyardlung@gmail.com>
2021-05-01 10:50:42 +0700

    feat: добавить хэширование пароля пользователя
    Расширил контроллер createUser. Пароль перед отправкой в базу данных
    хэшируется модулем bcryptjs.
    
    Расширил схему пользователя, добавив поля email и password. Валидность
    формата ввода email и avatar проверяется модулем validator. Поля name,
    about и avatar теперь необязательные; если на сервер пришли
    незаполненными, подставляются дефолтные значения.

--- models/user.js
+++ models/user.js
@@ -6,17 +6,23 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    validate: {
+      validator(url) {
+        return validator.isUrl(url);
+      },
+      message: 'Некорректный формат ссылки',
+    },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
